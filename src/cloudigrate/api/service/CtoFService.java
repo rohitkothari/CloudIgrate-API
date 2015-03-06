931e56692afefe7501a86d60622bab4aa3c4e2dc
Rohit Kothari <rohietkothari@gmail.com>
2015-03-05 18:54:27 -0800

    Removed createBucket bugs & updated REST actions

--- src/cloudigrate/api/service/CtoFService.java
+++ src/cloudigrate/api/service/CtoFService.java
@@ -1,15 +1,17 @@
 package cloudigrate.api.service;
 
 import cloudigrate.api.impl.StorageImpl;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
  
-@Path("/ctofservice")
+@Path("/storage")
 public class CtoFService {
-	@GET
+	/*@GET
 	@Produces("application/xml")
 	public String convertCtoF() {
  
@@ -19,8 +21,16 @@ public class CtoFService {
 		System.out.println("Test");
 		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
 		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
-	}
+	} */
+	
+	@GET
+	public Response convertCtoF() {
  
+		System.out.println("Welcome to CloudIgrate-Storage");
+		return Response.status(200).entity("Welcome to CloudIgrate-Storage").build();
+	}
+	
+ /*
 	@Path("{c}")
 	@GET
 	@Produces("application/xml")
@@ -31,17 +41,16 @@ public class CtoFService {
  
 		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
 		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
-	}
+	}*/
 	
-	@Path("/bucket/{bucket}")
+	@Path("{bucket}")
 	@GET
-	@Produces("application/xml")
-	public String createBucket(@PathParam("bucket") String s) {
+	public String createBucket(@PathParam("bucket") String bucket) {
 		
-		System.out.println("Inside bucket:"+s);
+		System.out.println("Inside bucket:"+bucket);
 		StorageImpl storage = new StorageImpl();
 		storage.authenticateAWS();
-		storage.createAWSBucket(s);
+		storage.createAWSBucket(bucket);
 		return "Bucket created successfully";
 	}
 }
