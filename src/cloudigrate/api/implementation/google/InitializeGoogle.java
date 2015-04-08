d0eb2f6e315e415a0401b1abf4940a0de2a1055a
rohitkothari <rohietkothari@gmail.com>
2015-04-07 22:58:06 -0700

    Update InitializeGoogle.java

--- src/cloudigrate/api/implementation/google/InitializeGoogle.java
+++ src/cloudigrate/api/implementation/google/InitializeGoogle.java
@@ -16,8 +16,8 @@ import com.google.api.services.storage.StorageScopes;
 
 public class InitializeGoogle {
 
-	private static final String ACCOUNT_ID_PROPERTY = "559680496685-uih56shoold7b7olrvt5smc01cbcdgu3@developer.gserviceaccount.com";
-	private static final String PRIVATE_KEY_PATH_PROPERTY = "/Users/rohietkothari/cloudigrate-poc-gcs-a9273c6661b4.p12";
+	private static final String ACCOUNT_ID_PROPERTY = "";
+	private static final String PRIVATE_KEY_PATH_PROPERTY = "";
 
 	public static Credential getCredentials() throws GeneralSecurityException, IOException
 	{
