91c35728538c5f5892e77c20056cd783ccbab014
vaibhavabhor <vaibhavabhor@gmail.com>
2015-04-14 13:49:38 -0700

    SQL flow updated with Key and logger
    Enabled auth key validation for SQL controller and logger.

--- src/cloudigrate/api/controller/SqlController.java
+++ src/cloudigrate/api/controller/SqlController.java
@@ -5,7 +5,12 @@
 package cloudigrate.api.controller;
 
 import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
+import cloudigrate.api.domain.Logger;
 import cloudigrate.api.facade.*;
 
 import javax.ws.rs.GET;
@@ -25,6 +30,12 @@ import javax.ws.rs.core.Response;
 public class SqlController {
 
 	SqlFacade sqlFacade = new SqlFacade();
+	AuthenticationFacade authFacade = new AuthenticationFacade();
+	Logger logger = null;
+	HashMap<String, String> myMap = null;
+	Iterator iterator = null;
+	int keyId = 0;
+	Map.Entry pair = null;
 
 	/*
 	 *  GET WelcomePage at /sql
@@ -43,19 +54,56 @@ public class SqlController {
 	@Path("/username")
 	@GET
 	public String getUserName(@QueryParam("authKey") String authKey) {
+		keyId = authFacade.isValidKey(authKey);
+		String username = null;
+		if(keyId > 0)
+		{
+		logger = Logger.getInstance();
+		logger.setStart(new Date());
+			myMap = authFacade.getLogInfo(keyId);
+			
+			System.out.println("Start is " + logger.getStart());
 			System.out.println("Inside SqlController - getUserName()");
-			return sqlFacade.getUserName();			
+			username = sqlFacade.getUserName();
+			logger.setEnd(new Date());
+			System.out.println("ENd is " + logger.getEnd());
+			
+			iterator = myMap.entrySet().iterator();
+		    while (iterator.hasNext()) {
+		    	pair = (Map.Entry)iterator.next();
+		        logger.writeLogger(pair.getKey().toString(), pair.getValue().toString(), "getUserName", "AWS", "sql", "PaaS");
+		        iterator.remove(); // avoids a ConcurrentModificationException
+		    }
 		}
+		return username;
+	}
 	
 	/*
 	 *  GET UserPassword at /sql
 	 */
 
-	@Path("/userpassword")
+	@Path("/password")
 	@GET
 	public String getUserPasword(@QueryParam("authKey") String authKey) {
+		keyId = authFacade.isValidKey(authKey);
+		String password = null;
+		if(keyId > 0)
+		{
+		logger = Logger.getInstance();
+		logger.setStart(new Date());
+			myMap = authFacade.getLogInfo(keyId);
+			
+			System.out.println("Start is " + logger.getStart());
 		System.out.println("Inside SqlController - getUserPassword()");
-		return sqlFacade.getUserPassword();		
+		password = sqlFacade.getUserPassword();
+		iterator = myMap.entrySet().iterator();
+	    while (iterator.hasNext()) {
+	    	pair = (Map.Entry)iterator.next();
+	        logger.writeLogger(pair.getKey().toString(), pair.getValue().toString(), "getUserPasword", "AWS", "sql", "PaaS");
+	        iterator.remove(); // avoids a ConcurrentModificationException
+	    }
+		}
+		return password;
 	}
 	
 	/*
@@ -65,7 +113,24 @@ public class SqlController {
 	@Path("/connectionstring")
 	@GET
 	public String getConnectionString(@QueryParam("authKey") String authKey) {
-		System.out.println("Inside SqlController - getPassword()");
-		return sqlFacade.getConnectionString();		
+		keyId = authFacade.isValidKey(authKey);
+		String connectionString = null;
+		if(keyId > 0)
+		{
+		logger = Logger.getInstance();
+		logger.setStart(new Date());
+			myMap = authFacade.getLogInfo(keyId);
+			
+			System.out.println("Start is " + logger.getStart());
+		System.out.println("Inside SqlController - getConnectionString()");
+		connectionString=  sqlFacade.getConnectionString();		
+		iterator = myMap.entrySet().iterator();
+	    while (iterator.hasNext()) {
+	    	pair = (Map.Entry)iterator.next();
+	        logger.writeLogger(pair.getKey().toString(), pair.getValue().toString(), "getConnectionString", "AWS", "sql", "PaaS");
+	        iterator.remove(); // avoids a ConcurrentModificationException
+	    }
+		}
+		return connectionString;
 	}
 }
